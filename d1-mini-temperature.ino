8cecaf2b2b4c7e9d05008f6df65274c24dcde016
lauwarm <10065855+lauwarm@users.noreply.github.com>
2022-04-16 18:21:16 +0200

    add rtc

--- d1-mini-temperature.ino
+++ d1-mini-temperature.ino
@@ -2,6 +2,14 @@
 #include <InfluxDbClient.h>
 #include <ESP8266WiFiMulti.h>
 ESP8266WiFiMulti wifiMulti;
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <RTClib.h>
+
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 
 #define WIFI_SSID ""
 #define WIFI_PASS ""
@@ -17,9 +25,33 @@ ESP8266WiFiMulti wifiMulti;
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
 Point sensor("wifi status");
 
+//Temperature Sensor
+const int oneWireBus = 2;
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
+  Serial.begin(57600);
+  
+  #ifndef ESP8266
+  while (!Serial); // wait for serial port to connect. Needed for native USB
+#endif
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 
   pinMode(LED_BUILTIN, OUTPUT); 
   digitalWrite(LED_BUILTIN, HIGH);
@@ -60,13 +92,16 @@ void loop() {
   //digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
   //delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
 
+  sensors.requestTemperatures();
+  delay(1000);
   // Store measured value into point
   sensor.clearFields();
   // Report RSSI of currently connected network
   sensor.addField("rssi", WiFi.RSSI());
+  sensor.addField("temperature", sensors.getTempCByIndex(0));
   // Print what are we exactly writing
-  Serial.print("Writing: ");
-  Serial.println(client.pointToLineProtocol(sensor));
+  //Serial.print("Writing: ");
+  //Serial.println(client.pointToLineProtocol(sensor));
   // If no Wifi signal, try to reconnect it
   if (wifiMulti.run() != WL_CONNECTED) {
     Serial.println("Wifi connection lost");
@@ -79,5 +114,52 @@ void loop() {
 
   //Wait 10s
   Serial.println("Wait 10s");
-  delay(10000);
+  delay(9000);
+
+      DateTime now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+
+    // calculate a date which is 7 days, 12 hours, 30 minutes, 6 seconds into the future
+    DateTime future (now + TimeSpan(7,12,30,6));
+
+    Serial.print(" now + 7d + 12h + 30m + 6s: ");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(' ');
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+
+    Serial.print("Temperature: ");
+    Serial.print(rtc.getTemperature());
+    Serial.println(" C");
+
+    Serial.println();
+    delay(3000);
 }
