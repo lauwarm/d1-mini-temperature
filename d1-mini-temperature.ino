883fca2a0ca83b95afb79541acba1ae5eb88ebb2
lauwarm <10065855+lauwarm@users.noreply.github.com>
2022-04-24 13:02:32 +0200

    cleaning up code

--- d1-mini-temperature.ino
+++ d1-mini-temperature.ino
@@ -1,90 +1,158 @@
 #include <ESP8266WiFi.h>
 #include <InfluxDbClient.h>
-#include <ESP8266WiFiMulti.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <RTClib.h>
 #include <NTPClient.h>
 #include <WifiUdp.h>
 
+const char* WIFI_SSID = "";
+const char* WIFI_PASS = "";
+
+const char* DEVICE = "ESP8266";
+const char* SENSOR = "DS18B20";
+const char* INFLUXDB_URL = ""; 
+//const char* INFLUXDB_TOKEN = ""
+//const char* INFLUXDB_ORG = ""
+const char* INFLUXDB_DB_NAME = "";
+const char* INFLUXDB_USER = "";
+const char* INFLUXDB_PASSWORD = "";
+
+const char daysOfTheWeek[7][12] = 
+  {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; // ---TESTING---
+
+// Realtime Clock
 RTC_DS3231 rtc;
-ESP8266WiFiMulti wifiMulti;
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+// NTP Client
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
-#define WIFI_SSID ""
-#define WIFI_PASS ""
-
-#define DEVICE "ESP8266"
-#define INFLUXDB_URL ""
-#define INFLUXDB_TOKEN ""
-#define INFLUXDB_ORG ""
-#define INFLUXDB_DB_NAME ""
-#define INFLUXDB_USER ""
-#define INFLUXDB_PASSWORD ""
-
+// InfluxDB Client
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
 Point sensor("wifi status");
 
-//Temperature Sensor
+// Temperature Sensor
 const int oneWireBus = 2; //D4
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
 
-#define CLOCK_INTERRUPT_PIN 16 //D0
+const int CLOCK_INTERRUPT_PIN = 16; // ---TESTING---
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+//---
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // ---TESTING---
   
-  #ifndef ESP8266
-  while (!Serial); // wait for serial port to connect. Needed for native USB
-  #endif
+  #ifndef ESP8266 // ---TESTING---
+  while (!Serial); // wait for serial port to connect. Needed for native USB // ---TESTING---
+  #endif // ---TESTING---
 
-  if (! rtc.begin()) {
+  checkRTC();
+
+  setWifi();
+  
+  debugMessages(1); // ---TESTING---
+
+  setInfluxDB();
+
+  setRTCTime();
+
+  setRTCAlarm();
+  pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP); // ---TESTING---
+  attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), onAlarm, FALLING); // ---TESTING---
+}
+
+void loop() {
+  //debugMessages(5); // ---TESTING---
+  //debugMessages(2); // ---TESTING---
+  
+  // Activate Sensor and wait a Second before reading Values
+  sensors.requestTemperatures();
+  delay(1000);
+  
+  // Store measured value into point
+  sensor.clearFields();
+  sensor.addField("rssi", WiFi.RSSI());
+  sensor.addField("temperature", sensors.getTempCByIndex(0));
+  
+  debugMessages(3); // ---TESTING---
+    
+  // Write point
+  if (!client.writePoint(sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  Serial.println("Clearing Alarm 1"); // ---TESTING---
+  // Clear Alarm 1 to Shutdown ESP8266
+  rtc.clearAlarm(1);
+  Serial.println("Alarm 1 cleared"); // ---TESTING---
+  
+  //while(1);
+  delay(15000);
+}
+
+ICACHE_RAM_ATTR void onAlarm() { // ---TESTING---
+  Serial.println("Alarm occured!"); // ---TESTING---
+} // ---TESTING---
+
+void checkRTC() {
+    if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     while (1) delay(10);
   }
-//  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  
-  
+}
 
+void setRTCTime() {
   if (rtc.lostPower()) {
-    Serial.println("RTC lost power, let's set the time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    Serial.println("RTC lost power, lets set the time!"); // ---TESTING---
+    timeClient.begin();
+    timeClient.setTimeOffset(0);
+    timeClient.update();
+    rtc.adjust(DateTime(timeClient.getEpochTime()));
   }
-  
-  pinMode(LED_BUILTIN, OUTPUT); 
-  digitalWrite(LED_BUILTIN, HIGH);
+}
 
+void setRTCAlarm() {
+  // Disable 32K
+  rtc.disable32K();
+
+  // Clear Alarm 1 and 2
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
+
+  // Disable SQW Pin
+  rtc.writeSqwPinMode(DS3231_OFF);
+
+  // Disable Alarm 2
+  rtc.disableAlarm(2);
+
+  // Set Alarm 1 to current Time + a Timespan in Seconds
+  if(!rtc.setAlarm1(rtc.now() + TimeSpan(600),DS3231_A1_Second)) {
+    Serial.println("Error, alarm wasn't set!");
+  }
+  else {
+    Serial.println("Alarm will happen in 600 seconds!");  
+  }
+}
+
+void setWifi() {
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-  Serial.print("Connecting to: ");
-  Serial.print(WIFI_SSID);
+  debugMessages(4);
 
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+}
 
-  Serial.println();
-  Serial.print("Connected! IP: ");
-  Serial.print(WiFi.localIP());
-
-  //ntp stuff and set rtc
-  timeClient.begin();
-  timeClient.setTimeOffset(0);
-  timeClient.update();
-  rtc.adjust(DateTime(timeClient.getEpochTime()));
-
+void setInfluxDB() {
   client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DB_NAME, INFLUXDB_USER, INFLUXDB_PASSWORD);
-
   sensor.addTag("device", DEVICE);
   sensor.addTag("SSID", WiFi.SSID());
-
   if (client.validateConnection()) {
     Serial.print("Connected to InfluxDB: ");
     Serial.println(client.getServerUrl());
@@ -92,98 +160,70 @@ void setup() {
     Serial.print("InfluxDB connection failed: ");
     Serial.println(client.getLastErrorMessage());
   }
-
-  rtc.disable32K(); // RTC
-  pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), onAlarm, FALLING);
-  rtc.clearAlarm(1);
-  rtc.clearAlarm(2);
-  rtc.writeSqwPinMode(DS3231_OFF);
-  rtc.disableAlarm(2);
-  if(!rtc.setAlarm1(rtc.now() + TimeSpan(10),DS3231_A1_Second)) {
-    Serial.println("Error, alarm wasn't set!");
-  }
-  else {
-    Serial.println("Alarm will happen in 10 seconds!");  
-  }
 }
 
-void loop() {
-  //digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  // but actually the LED is on; this is because
-  // it is active low on the ESP-01)
-  //delay(1000);                      // Wait for a second
-  //digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  //delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+void debugMessages(int choose) { // ---TESTING---
+  switch (choose) {
+    case 1:
+    {
+      Serial.println(); // ---TESTING---
+      Serial.print("Connected! IP: "); // ---TESTING---
+      Serial.print(WiFi.localIP()); // ---TESTING---
+      Serial.println();
+      break;
+    }
+    case 2:
+    {
+      //RTC MAGIC
+      DateTime now = rtc.now(); // ---TESTING---
+      Serial.print("Formated Time: "); // ---TESTING---
+      Serial.println(); // ---TESTING---
+      Serial.print(now.year(), DEC); // ---TESTING---
+      Serial.print('/'); // ---TESTING---
+      Serial.print(now.month(), DEC); // ---TESTING---
+      Serial.print('/'); // ---TESTING---
+      Serial.print(now.day(), DEC); // ---TESTING---
+      Serial.print(" ("); // ---TESTING---
+      Serial.print(daysOfTheWeek[now.dayOfTheWeek()]); // ---TESTING---
+      Serial.print(") "); // ---TESTING---
+      Serial.print(now.hour(), DEC);// ---TESTING---
+      Serial.print(':'); // ---TESTING---
+      Serial.print(now.minute(), DEC); // ---TESTING---
+      Serial.print(':'); // ---TESTING---
+      Serial.print(now.second(), DEC); // ---TESTING---
+      Serial.println(); // ---TESTING---
+      Serial.print(" since midnight 1/1/1970 = "); // ---TESTING---
+      Serial.print(now.unixtime()); // ---TESTING---
+      Serial.print("s = "); // ---TESTING---
+      Serial.print(now.unixtime() / 86400L); // ---TESTING---
+      Serial.println("d"); // ---TESTING---
+      Serial.println(); // ---TESTING---
+      break;
+    }
+    case 3:
+    {
+      // Print what are we exactly writing
+      Serial.print("Writing: "); // ---TESTING---
+      Serial.println(client.pointToLineProtocol(sensor)); // ---TESTING---  
+      break;
+    }
+    case 4:
+    {
+      Serial.print("Connecting to: "); // ---TESTING---
+      Serial.print(WIFI_SSID); // ---TESTING---
+      break;
+    }
+    case 5:
+    {
+      Serial.print("Lost Power? - "); // ---TESTING---
+      Serial.println(rtc.lostPower()); // ---TESTING---
 
-
-  //RTC MAGIC
-  DateTime now = rtc.now();
-  Serial.println();
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-
-  Serial.print(" since midnight 1/1/1970 = ");
-  Serial.print(now.unixtime());
-  Serial.print("s = ");
-  Serial.print(now.unixtime() / 86400L);
-  Serial.println("d");
-
-  Serial.println();
-
-  Serial.print("SQW: ");
-  Serial.print(digitalRead(CLOCK_INTERRUPT_PIN));
-  Serial.print(" Alarm1: ");
-  Serial.print(rtc.alarmFired(1));
-  Serial.println();
-  
-  if(rtc.alarmFired(1)) {
-    rtc.clearAlarm(1);
-    Serial.println("Alarm cleared!");
-
-    //TEST
-      timeClient.update();
-  Serial.print("Formated Time: ");
-  Serial.print(timeClient.getFormattedTime());
-  Serial.println();
-  sensors.requestTemperatures();
-  Serial.println("Wait 1s for Temperature Sensor");
-  delay(1000);
-  Serial.println(sensors.getTempCByIndex(0));
-  // Store measured value into point
-  sensor.clearFields();
-  // Report RSSI of currently connected network
-  sensor.addField("rssi", WiFi.RSSI());
-  sensor.addField("temperature", sensors.getTempCByIndex(0));
-  // Print what are we exactly writing
-  //Serial.print("Writing: ");
-  //Serial.println(client.pointToLineProtocol(sensor));
-  // If no Wifi signal, try to reconnect it
-  if (wifiMulti.run() != WL_CONNECTED) {
-    Serial.println("Wifi connection lost");
+      Serial.print("SQW: "); // ---TESTING---
+      Serial.print(digitalRead(CLOCK_INTERRUPT_PIN)); // ---TESTING---
+      Serial.print(" Alarm1: "); // ---TESTING---
+      Serial.print(rtc.alarmFired(1)); // ---TESTING---
+      Serial.println(); // ---TESTING---
+      break;
+    }
   }
-  // Write point
-  if (!client.writePoint(sensor)) {
-    Serial.print("InfluxDB write failed: ");
-    Serial.println(client.getLastErrorMessage());
-  }
-  }
-  
-  delay(9000);
-}
-
-ICACHE_RAM_ATTR void onAlarm() {
-  Serial.println("Alarm occured!");
 }
